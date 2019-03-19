ffd23f59edc0e27fec219d13c7bf5270c4714e6a
conconrob <connortprobertson@gmail.com>
2019-03-19 13:14:24 +0000

    Coded and tested delete endpoint

--- routesPosts.js
+++ routesPosts.js
@@ -35,18 +35,36 @@ routes.get("/", (req, res) => {
     });
 });
 
-routes.get("/:id",(req, res) => {
+routes.get("/:id", (req, res) => {
   Posts.findById(req.params.id)
     .then(data => {
-      if(data.length > 0){
-        res.status(200).json(data)
-      }else {
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
+      if (data.length > 0) {
+        res.status(200).json(data);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
       }
     })
     .catch(() => {
-      res.status(500).json({ error: "The post could not be removed" })
+      res.status(500).json({ error: "The post could not be removed" });
+    });
+});
+
+routes.delete("/:id", (req, res) => {
+  Posts.remove(req.params.id)
+    .then(data => {
+      if (data) {
+        res.status(202).json(req.params.id);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
     })
-})
+    .catch(() => {
+      res.status(500).json({ error: "The post could not be removed" });
+    });
+});
 
 module.exports = routes;
